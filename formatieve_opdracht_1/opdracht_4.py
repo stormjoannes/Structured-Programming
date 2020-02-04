5c5e6db4d472c190b7cc143ff87ddb02d75d1adc
Storm Joannes <storm.joannes@gmail.com>
2020-02-04 18:20:10 +0100

    storm

--- formatieve_opdracht_1/opdracht_4.py
+++ formatieve_opdracht_1/opdracht_4.py
@@ -1,11 +1,18 @@
-# def palindroome():
-#     x = input('geef een woord: ')
-#     a = x.reverse()
-#     if a == x:
-#         return 'Wel een palindroom'
-#     else:
-#         return 'Niet een palindroom'
-# print(palindroome())
+#ik zag het nut van opdr 4 om een built in function te gebruiken niet maar ik dacht ik gebruik reverse wel op een rare manier.
+def palindroome():
+    x = input('geef een woord: ')
+    lst = []
+    for i in x:
+        lst.append(i)
+    lst.reverse()
+    lstheel = []
+    for i in x:
+        lstheel.append(i)
+    if lst == lstheel:
+        return 'Wel een palindroom'
+    else:
+        return 'Niet een palindroom'
+print(palindroome())
 
 print('')
 
--- formatieve_opdracht_1/opdracht_5.py
+++ formatieve_opdracht_1/opdracht_5.py
@@ -1,4 +1,5 @@
 lst = [4, 6, 6, 2, 5, 4, 7, 1, 2, 3, 9, 4, 5, 7, 4, 2, 1, 1, 1, 6, 5]
+#bij deze opdracht heb ik twee methodes gebruikt. Eentjes met swappen die we bij een ander vak al hadden gemaakt, en eentje waarbij je een nieuwe lijst creëert en met minimaal werkt.
 def sort(lst):
     lengte = len(lst)
     for i in range(lengte):
@@ -7,3 +8,18 @@ def sort(lst):
                 lst[x], lst[x + 1] = lst[x + 1], lst[x]
     return lst
 print(sort(lst))
+
+
+def asort(lst):
+    nlst = []
+    while (len(lst) != 0):
+        for i in range(0, len(lst)):
+            if i == 0:
+                min = lst[i]
+            else:
+                if lst[i] < min:
+                    min = lst[i]
+        nlst.append(min)
+        lst.remove(min)
+    return nlst
+print(asort(lst))
