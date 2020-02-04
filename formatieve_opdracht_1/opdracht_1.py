ba262e6a0656bb6052f703977a4659b7eb4a1de4
Storm Joannes <storm.joannes@gmail.com>
2020-02-04 14:38:56 +0100

    hoi

---
+++ formatieve_opdracht_1/opdracht_1.py
@@ -0,0 +1,40 @@
+k = int(input('hoe groot:'))
+for i in range (1, k + 1):
+    print(i * '*')
+
+for x in range (1, k):
+    print((k - x) * '*')
+
+print ( '')
+
+ka = 0
+z = k
+while z != 0:
+    ka += 1
+    z -= 1
+    print(ka * '*')
+
+j = k
+while j != 0:
+    j -= 1
+    print(j * '*')
+
+ko = 0
+b = k
+while b != 0:
+    ko += 1
+    b -= 1
+    print(b * ' ' + (ko * '*'))
+
+ha = k
+while ha != 0:
+    ha -= 1
+    print((k - ha) * ' ' + (ha * '*'))
+
+for i in range (1, k + 1):
+    print((k - i) * ' ' + (i * '*'))
+
+haak = k
+for x in range (k):
+    haak -= 1
+    print((k - haak) * ' ' + (haak * '*'))
