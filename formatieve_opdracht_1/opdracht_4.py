6d0f6bf6c0b8e9defaa2a9dc31d8adc66d985420
Storm Joannes <60653315+stormjoannes@users.noreply.github.com>
2020-02-05 20:11:05 +0100

    Update opdracht_4.py

--- formatieve_opdracht_1/opdracht_4.py
+++ formatieve_opdracht_1/opdracht_4.py
@@ -1,14 +1,7 @@
-#ik zag het nut van opdr 4 om een built in function te gebruiken niet in maar ik dacht ik gebruik reverse wel op een rare manier.
 def palindroome():
-    x = input('geef een woord: ')
-    lst = []
-    for i in x:
-        lst.append(i)
-    lst.reverse()
-    lstheel = []
-    for i in x:
-        lstheel.append(i)
-    if lst == lstheel:
+    x = input('Type een woord: ')
+    s = ''.join(reversed(x))
+    if s == x:
         return 'Wel een palindroom'
     else:
         return 'Niet een palindroom'
