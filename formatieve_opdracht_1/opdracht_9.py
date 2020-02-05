6461b351c857a54092816120e1a240004f0f47d1
Storm Joannes <storm.joannes@gmail.com>
2020-02-05 21:18:06 +0100

    storm

--- formatieve_opdracht_1/opdracht_9.py
+++ formatieve_opdracht_1/opdracht_9.py
@@ -3,8 +3,5 @@ ch = '1011000'
 n = int(input('geef de bitwaarde: '))
 
 def verschuiven(ch, n):
-    if n < 0:
-        return ch[n:] + ch[:n]
-    else:
-        return ch[n:] + ch[:n]
+    return ch[n:] + ch[:n]
 print(verschuiven(ch, n))
